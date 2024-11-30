33097bc201b596f3bb5e6697f84bcfe41243886c
Kin Fei Lee <kilee@seekasia.com>
2024-11-30 21:07:21 +0800

    Initial commit

---
+++ bin/cdk-workshop.ts
@@ -0,0 +1,6 @@
+#!/opt/homebrew/opt/node/bin/node
+import * as cdk from 'aws-cdk-lib';
+import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+
+const app = new cdk.App();
+new CdkWorkshopStack(app, 'CdkWorkshopStack');
