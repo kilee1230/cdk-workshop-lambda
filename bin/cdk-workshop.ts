55d2cd846dbd7fece67b4607adac5551112fd619
Kin Fei Lee <kilee@seekasia.com>
2024-12-17 10:54:31 +0800

    feat: update stack name

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -4,4 +4,6 @@ import * as cdk from "aws-cdk-lib";
 import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 
 const app = new cdk.App();
-new CdkWorkshopStack(app, "CdkWorkshopStack");
+new CdkWorkshopStack(app, "CdkWorkshopStack", {
+  stackName: "cdk-workshop-lambda",
+});
